1ca9944e37435aa2533f1a763e2602d14c5e0991
Anjali Tandon <anjalitandon@Anjalis-MacBook-Air-3.local>
2023-12-07 17:32:50 -0500

    anjali

--- main_muse.py
+++ main_muse.py
@@ -1,5 +1,3 @@
-# please fix this code such that the amount of data stored from an API into the database each time i execute the file is 25 or fewer items. what this means is that my database insertion statements should be such that i only add 25 rows of data into each table every time i execute my program. The purpose behind this is to never flood a database with insert requests, as when working with data in the real world, this could cause disruptions within a service or company.
-
 import sqlite3
 import requests
 from datetime import datetime, timedelta
