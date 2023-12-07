703906f17f354f7353939013ff4f05b9a2e86aab
Anjali Tandon <anjalitandon@Anjalis-MacBook-Air-3.local>
2023-12-07 16:29:39 -0500

    better version i think

--- anjali-reddit-scraping.py
+++ anjali-reddit-scraping.py
@@ -1,15 +1,18 @@
-# import requests
-# from pprint import pprint
-# full_data = []
-# for i in range(10)
-#       res = requests.get(https://www.themuse.com/api/public/jobs?page=i)
-#       data = res.json()
+import requests
+import sqlite3
+from datetime import datetime
 
-#       lists = data['results']
-#       if data['publication_date'] < <SOME VALUE>:
-#           for item in lists:
-#               name = item['name']
-#               full_data.append(name)
+from pprint import pprint
+full_data = []
+for i in range(10):
+      res = requests.get('https://www.themuse.com/api/public/jobs?page=' + i)
+      data = res.json()
+
+      lists = data['results']
+      if data['publication_date'] > '2023-05-19T23:38:55Z':
+          for item in lists:
+              name = item['name']
+              full_data.append(name)
 
 # pprint(data)
 
