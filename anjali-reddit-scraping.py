85bd0b9561a3eb2af823d878d7e19aa758f7fac3
Anjali Tandon <anjalitandon@Anjalis-MacBook-Air-3.local>
2023-12-07 17:08:52 -0500

    anjali

--- anjali-reddit-scraping.py
+++ anjali-reddit-scraping.py
@@ -1,20 +1,28 @@
 import requests
-import sqlite3
-from datetime import datetime
-
+from datetime import datetime, timedelta
 from pprint import pprint
+
 full_data = []
-for i in range(10):
-      res = requests.get('https://www.themuse.com/api/public/jobs?page=' + i)
-      data = res.json()
+page_count = 0
 
-      lists = data['results']
-      if data['publication_date'] > '2023-05-19T23:38:55Z':
-          for item in lists:
-              name = item['name']
-              full_data.append(name)
+while len(full_data) < 100 and page_count < 25:
+    page_count += 1
+    res = requests.get(f'https://www.themuse.com/api/public/jobs?page={page_count}')
+    data = res.json()
 
-# pprint(data)
+    lists = data['results']
+    for item in lists:
+        # Convert the publication date to a datetime object
+        publication_date = datetime.strptime(item['publication_date'], '%Y-%m-%dT%H:%M:%SZ')
+
+        # Check if the publication date is after '2023-05-19T23:38:55Z'
+        if publication_date > datetime.strptime('2023-05-19T23:38:55Z', '%Y-%m-%dT%H:%M:%SZ'):
+            name = item['name']
+            full_data.append(name)
+
+# Print the collected data
+pprint(full_data)
+pprint(len(full_data))
 
 # for i in range(25):
 #   cur.execute('INSERT ...', full_data[i])
