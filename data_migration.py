ce9b29facb42a403fae39753cb5eff8b14bd987f
Olive Ren <oliveren@Olives-MacBook-Air.local>
2023-12-09 16:15:48 -0500

    OMG one database

--- data_migration.py
+++ data_migration.py
@@ -1,8 +1,3 @@
-# This script migrates data from the Muse jobs database, the LinkedIn jobs database, and the News API database into a unified database.
-# next steps: I have done calculations for news api in data_processing.py, but I have not done calculations for muse and linkedin.
-# To run the code again i think i need to go to main.py to fetch data from news.api and run data_migration.py and data_processing.py
-# 
-# Run this script to get a unified databaseå
 import sqlite3
 
 def create_table_in_unified_db(db_path, create_table_sql):
@@ -27,7 +22,39 @@ def insert_data_into_unified_db(db_path, data, insert_query):
     conn.commit()
     conn.close()
 
-# Correct the CREATE TABLE statements to match your source table structures
+# Create tables for LinkedIn data
+create_table_in_unified_db('unified_database.db', '''
+    CREATE TABLE IF NOT EXISTS linkedin_titles (
+        id INTEGER PRIMARY KEY,
+        name TEXT UNIQUE
+    )
+''')
+create_table_in_unified_db('unified_database.db', '''
+    CREATE TABLE IF NOT EXISTS linkedin_companies (
+        id INTEGER PRIMARY KEY,
+        name TEXT UNIQUE
+    )
+''')
+create_table_in_unified_db('unified_database.db', '''
+    CREATE TABLE IF NOT EXISTS linkedin_locations (
+        id INTEGER PRIMARY KEY,
+        name TEXT UNIQUE
+    )
+''')
+create_table_in_unified_db('unified_database.db', '''
+    CREATE TABLE IF NOT EXISTS linkedin_jobs (
+        job_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title_id INTEGER,
+        company_id INTEGER,
+        location_id INTEGER,
+        apply_link TEXT,
+        FOREIGN KEY (title_id) REFERENCES linkedin_titles (id),
+        FOREIGN KEY (company_id) REFERENCES linkedin_companies (id),
+        FOREIGN KEY (location_id) REFERENCES linkedin_locations (id)
+    )
+''')
+
+# Create tables for Muse data
 create_table_in_unified_db('unified_database.db', '''
     CREATE TABLE IF NOT EXISTS muse_job_postings (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,16 +65,7 @@ create_table_in_unified_db('unified_database.db', '''
     )
 ''')
 
-create_table_in_unified_db('unified_database.db', '''
-    CREATE TABLE IF NOT EXISTS linkedin_jobs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        company TEXT,
-        location TEXT,
-        apply_link TEXT
-    )
-''')
-#news_api_table1
+# Create tables for News API data
 create_table_in_unified_db('unified_database.db', '''
     CREATE TABLE IF NOT EXISTS news_posts (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -57,46 +75,40 @@ create_table_in_unified_db('unified_database.db', '''
         neutral_count INTEGER,
         positive_count INTEGER,
         negative_count INTEGER
-        
     )
 ''')
-#news_api_table2
 create_table_in_unified_db('unified_database.db', '''
     CREATE TABLE IF NOT EXISTS news_sources (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         source_id INTEGER,
         name TEXT
-        
     )
 ''')
 
-# Fetch and insert data from the first News API table
-news_posts_data = fetch_data_from_db('news_database.db', 'SELECT source_id, content, timestamp, neutral_count, positive_count, negative_count FROM posts')
-insert_data_into_unified_db('unified_database.db', news_posts_data, '''
-    INSERT INTO news_posts (source_id, content, timestamp, neutral_count, positive_count, negative_count) VALUES (?, ?, ?, ?, ?, ?)
-''')
-# Fetch and insert data from the second News API table
-news_sources_data = fetch_data_from_db('news_database.db', 'SELECT source_id, name FROM sources')
-insert_data_into_unified_db('unified_database.db', news_sources_data, '''
-    INSERT INTO news_sources (source_id, name) VALUES (?, ?)
-''')
+# Insert data into LinkedIn tables
+titles_data = fetch_data_from_db('linkedin-jobs.db', 'SELECT id, name FROM titles')
+companies_data = fetch_data_from_db('linkedin-jobs.db', 'SELECT id, name FROM companies')
+locations_data = fetch_data_from_db('linkedin-jobs.db', 'SELECT id, name FROM locations')
+jobs_data = fetch_data_from_db('linkedin-jobs.db', 'SELECT title_id, company_id, location_id, apply_link FROM jobs')
 
+insert_data_into_unified_db('unified_database.db', titles_data, 'INSERT INTO linkedin_titles (id, name) VALUES (?, ?)')
+insert_data_into_unified_db('unified_database.db', companies_data, 'INSERT INTO linkedin_companies (id, name) VALUES (?, ?)')
+insert_data_into_unified_db('unified_database.db', locations_data, 'INSERT INTO linkedin_locations (id, name) VALUES (?, ?)')
+insert_data_into_unified_db('unified_database.db', jobs_data, 'INSERT INTO linkedin_jobs (title_id, company_id, location_id, apply_link) VALUES (?, ?, ?, ?)')
 
-
-# Fetch and insert data from the Muse jobs database
+# Insert data into Muse table
 muse_data = fetch_data_from_db('muse_jobs.db', 'SELECT job_category, job_listing, publication_date, experience_level FROM job_postings')
 insert_data_into_unified_db('unified_database.db', muse_data, '''
     INSERT INTO muse_job_postings (job_category, job_listing, publication_date, experience_level) VALUES (?, ?, ?, ?)
 ''')
 
-# Fetch and insert data from the LinkedIn jobs database
-linkedin_data = fetch_data_from_db('linkedin-jobs.db', '''
-    SELECT j.title, c.name, l.name, j.apply_link 
-    FROM jobs j
-    INNER JOIN companies c ON j.company_id = c.id
-    INNER JOIN locations l ON j.location_id = l.id
-''')
-insert_data_into_unified_db('unified_database.db', linkedin_data, '''
-    INSERT INTO linkedin_jobs (title, company, location, apply_link) VALUES (?, ?, ?, ?)
-''')
+# Insert data into News API tables
+news_posts_data = fetch_data_from_db('news_database.db', 'SELECT source_id, content, timestamp, neutral_count, positive_count, negative_count FROM posts')
+news_sources_data = fetch_data_from_db('news_database.db', 'SELECT source_id, name FROM sources')
 
+insert_data_into_unified_db('unified_database.db', news_posts_data, '''
+    INSERT INTO news_posts (source_id, content, timestamp, neutral_count, positive_count, negative_count) VALUES (?, ?, ?, ?, ?, ?)
+''')
+insert_data_into_unified_db('unified_database.db', news_sources_data, '''
+    INSERT INTO news_sources (source_id, name) VALUES (?, ?)
+''')
