01667770d634ed92c26e6ebbc900afa6aafc66c4
SamVariyam <varis260@newschool.edu>
2023-12-10 18:14:24 -0500

    samyuktha commits

---
+++ linkedin_calculations.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+def calculate_top_job_titles(cursor, limit=5):
+    cursor.execute('''
+        SELECT titles.name, COUNT(jobs.job_id) as job_count
+        FROM titles
+        LEFT JOIN jobs ON titles.id = jobs.title_id
+        GROUP BY titles.name
+        ORDER BY job_count DESC
+        LIMIT ?
+    ''', (limit,))
+    top_job_titles = cursor.fetchall()
+
+    return top_job_titles
+
+def calculate_top_locations(cursor, limit=5):
+    cursor.execute('''
+        SELECT locations.name, COUNT(jobs.job_id) as job_count
+        FROM locations
+        LEFT JOIN jobs ON locations.id = jobs.location_id
+        GROUP BY locations.name
+        ORDER BY job_count DESC
+        LIMIT ?
+    ''', (limit,))
+    top_locations = cursor.fetchall()
+
+    return top_locations
+
+def write_to_txt_titles(data, filename='top_job_titles.txt'):
+    with open(filename, 'w') as file:
+        for title, count in data:
+            file.write(f"{title}: {count}\n")
+
+def write_to_txt_locations(data, filename='top_locations.txt'):
+    with open(filename, 'w') as file:
+        for location, count in data:
+            file.write(f"{location}: {count}\n")
+
+def main():
+    conn = sqlite3.connect('linkedin-jobs.db')
+    cursor = conn.cursor()
+
+     # Calculate top job titles
+    top_job_titles = calculate_top_job_titles(cursor)
+    write_to_txt_titles(top_job_titles, filename='top_job_titles.txt')
+
+    # Calculate top locations
+    top_locations = calculate_top_locations(cursor)
+    write_to_txt_locations(top_locations, filename='top_locations.txt')
+
+    conn.close()
+
+if __name__ == '__main__':
+    main()
---
+++ linkedin_visualizations.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def read_from_txt(filename='top_job_titles.txt'):
+    with open(filename, 'r') as file:
+        data = [line.strip().split(': ') for line in file]
+
+    return data
+
+def plot_top_job_titles(data):
+    titles, counts = zip(*data)
+    plt.bar(titles, counts)
+    plt.xlabel('Job Titles')
+    plt.ylabel('Number of Jobs')
+    plt.title('Top 5 Job Titles by Count')
+    plt.xticks(rotation=45, ha='right')
+    plt.ylim(bottom=0)
+    plt.tight_layout()
+    plt.show()
+
+def plot_top_locations(data):
+    sns.set(style='whitegrid')
+    plt.figure(figsize=(10, 6))
+
+
+    locations, counts = zip(*data)
+    plt.bar(locations, counts, color=sns.color_palette("viridis", len(locations)))
+    plt.xlabel('Locations', fontsize=12)
+    plt.ylabel('Number of Jobs', fontsize=12)
+    plt.title('Top 5 Job Locations by Count', fontsize=16)
+    plt.xticks(rotation=45, ha='right', fontsize=12)
+    plt.ylim(bottom=0)
+    plt.tight_layout()
+
+    plt.gca().invert_yaxis()
+    
+    for i, count in enumerate(counts):
+        plt.text(i, float(count) + 0.1, str(int(float(count))), ha='center', va='bottom', fontsize=10)
+
+    plt.show()
+
+def main():
+    job_titles_data = read_from_txt(filename='top_job_titles.txt')
+    plot_top_job_titles(job_titles_data)
+
+    # Visualize top locations
+    locations_data = read_from_txt(filename='top_locations.txt')
+    plot_top_locations(locations_data)
+
+if __name__ == '__main__':
+    main()
